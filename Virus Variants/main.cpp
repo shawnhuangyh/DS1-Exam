a56fe2649df70e622758732371f8f8443e610d75
Shawn Huang <shawn.huang.y.h@gmail.com>
2022-04-11 17:13:25 +0800

    chore: add test program

--- Virus Variants/main.cpp
+++ Virus Variants/main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include "DisjointSet.h"
+#include "Graph.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m, p, q;
+    char c;
+    cin >> n >> m;
+    DisjointSet<int> ds(n);
+    Graph<int> g(n);
+    for (int i = 0; i < m; i++) {
+        cin >> c >> p >> q;
+        switch (c) {
+            case 'S':
+                ds.Union(p, q);
+                break;
+            case 'H':
+                g.AddArc(p, q);
+                g.FindArc(p, q, ds);
+                g.FindArc(q, p, ds);
+                break;
+            default:
+                cout << "Invalid Input!" << endl;
+                i--;
+                break;
+        }
+    }
+    cout << ds.GetCount() << endl;
     return 0;
 }
