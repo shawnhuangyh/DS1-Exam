6a6fe60755c6570765dc1c67ade46173b796ef9a
Shawn Huang <shawn.huang.y.h@gmail.com>
2022-04-16 11:22:05 +0800

    revert: start using sequence queue

--- PCR Test/Queue.h
+++ PCR Test/Queue.h
@@ -13,10 +13,16 @@ using namespace std;
 
 class Queue {
 private:
-    Node<Person> *head, *front, *rear;
+    int front, rear;
+
+    int maxsize;
+
+    Person *elems;
 public:
 
     Queue();
+
+    Queue(int max);
 
     Queue(const Queue &copy);
 
@@ -24,20 +30,19 @@ public:
 
     ~Queue();
 
-    void Clear();
-
     bool EnQueue(Person elem);
 
     bool DeQueue(Person &elem);
 
-    bool IsEmpty();
+    bool IsEmpty() const;
+
+    bool IsFull() const;
 
     void ShowQueue();
 
-    Person FindElem(string elem);
+    Person FindElem(const string& elem);
 
 };
-
 
 
 #endif //PCR_TEST_QUEUE_H
